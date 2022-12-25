bc3fd88b7f3bdf8fa0c3225a87b05f7ccd106de1
pratik.bhagat <pratik.bhagat@netcorecloud.com>
2022-12-25 18:34:33 +0530

    initial commit

---
+++ src/main/java/com/springboot/spring/data/jpa/dao/EmployeeRepository.java
@@ -0,0 +1,24 @@
+package com.springboot.spring.data.jpa.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.spring.data.jpa.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+
+}
+
+
+
+
+
+
+
+
+
+
+
